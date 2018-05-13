8c9d7bb1fa854f65e9b3e528a329e2c34e9069e4
marc.thomas <marc.thomas@connect-group.com>
2018-05-13 21:17:13 +0100

    First commit

---
+++ src/main/java/com/amazonaws/lambda/demo/LambdaFunctionHandler.java
@@ -0,0 +1,13 @@
+package com.amazonaws.lambda.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaFunctionHandler implements RequestHandler<String, String> {
+
+    @Override
+    public String handleRequest(String request, Context context) {
+        return request.toUpperCase();
+    }
+
+}
